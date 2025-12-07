735e53cc9f52c0c4d15802154bb1885f08a5e150
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-12-07 23:16:38 +0100

    use TableFlowPanel for trigger/action section
    evenly splits the content 50/50

--- Key2Joy.Gui/MappingControl.Designer.cs
+++ Key2Joy.Gui/MappingControl.Designer.cs
@@ -29,30 +29,44 @@ partial class MappingControl
     private void InitializeComponent()
     {
             this.grpAction = new System.Windows.Forms.GroupBox();
+            this.actionControl = new Key2Joy.Gui.Mapping.ActionControl();
             this.btnSaveMapping = new System.Windows.Forms.Button();
             this.grpTrigger = new System.Windows.Forms.GroupBox();
+            this.triggerControl = new Key2Joy.Gui.Mapping.TriggerControl();
             this.chkCreateReverseMapping = new System.Windows.Forms.CheckBox();
             this.lblHeader = new System.Windows.Forms.Label();
-            this.triggerControl = new Key2Joy.Gui.Mapping.TriggerControl();
-            this.actionControl = new Key2Joy.Gui.Mapping.ActionControl();
+            this.tlpContainer = new System.Windows.Forms.TableLayoutPanel();
             this.grpAction.SuspendLayout();
             this.grpTrigger.SuspendLayout();
+            this.tlpContainer.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpAction
             // 
-            this.grpAction.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.grpAction.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.grpAction.Controls.Add(this.actionControl);
-            this.grpAction.Location = new System.Drawing.Point(538, 40);
+            this.grpAction.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpAction.Location = new System.Drawing.Point(516, 4);
             this.grpAction.Margin = new System.Windows.Forms.Padding(4);
             this.grpAction.Name = "grpAction";
             this.grpAction.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
-            this.grpAction.Size = new System.Drawing.Size(500, 160);
+            this.grpAction.Size = new System.Drawing.Size(504, 146);
             this.grpAction.TabIndex = 88;
             this.grpAction.TabStop = false;
             this.grpAction.Text = "Action";
+            // 
+            // actionControl
+            // 
+            this.actionControl.AutoSize = true;
+            this.actionControl.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.actionControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.actionControl.IsTopLevel = false;
+            this.actionControl.Location = new System.Drawing.Point(7, 21);
+            this.actionControl.Margin = new System.Windows.Forms.Padding(5);
+            this.actionControl.MinimumSize = new System.Drawing.Size(400, 39);
+            this.actionControl.Name = "actionControl";
+            this.actionControl.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.actionControl.Size = new System.Drawing.Size(490, 119);
+            this.actionControl.TabIndex = 0;
             // 
             // btnSaveMapping
             // 
@@ -68,18 +82,29 @@ partial class MappingControl
             // 
             // grpTrigger
             // 
-            this.grpTrigger.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.grpTrigger.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.grpTrigger.Controls.Add(this.triggerControl);
-            this.grpTrigger.Location = new System.Drawing.Point(5, 40);
+            this.grpTrigger.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpTrigger.Location = new System.Drawing.Point(4, 4);
             this.grpTrigger.Margin = new System.Windows.Forms.Padding(4);
             this.grpTrigger.Name = "grpTrigger";
             this.grpTrigger.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
-            this.grpTrigger.Size = new System.Drawing.Size(525, 160);
+            this.grpTrigger.Size = new System.Drawing.Size(504, 146);
             this.grpTrigger.TabIndex = 86;
             this.grpTrigger.TabStop = false;
             this.grpTrigger.Text = "Trigger";
+            // 
+            // triggerControl
+            // 
+            this.triggerControl.AutoSize = true;
+            this.triggerControl.BackColor = System.Drawing.SystemColors.Control;
+            this.triggerControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.triggerControl.IsTopLevel = false;
+            this.triggerControl.Location = new System.Drawing.Point(7, 21);
+            this.triggerControl.Margin = new System.Windows.Forms.Padding(5);
+            this.triggerControl.Name = "triggerControl";
+            this.triggerControl.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.triggerControl.Size = new System.Drawing.Size(490, 119);
+            this.triggerControl.TabIndex = 0;
             // 
             // chkCreateReverseMapping
             // 
@@ -105,40 +130,30 @@ partial class MappingControl
             this.lblHeader.TabIndex = 92;
             this.lblHeader.Text = "Editing Mapping";
             // 
-            // triggerControl
+            // tlpContainer
             // 
-            this.triggerControl.AutoSize = true;
-            this.triggerControl.BackColor = System.Drawing.SystemColors.Control;
-            this.triggerControl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.triggerControl.IsTopLevel = false;
-            this.triggerControl.Location = new System.Drawing.Point(7, 21);
-            this.triggerControl.Margin = new System.Windows.Forms.Padding(5);
-            this.triggerControl.Name = "triggerControl";
-            this.triggerControl.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
-            this.triggerControl.Size = new System.Drawing.Size(511, 133);
-            this.triggerControl.TabIndex = 0;
-            // 
-            // actionControl
-            // 
-            this.actionControl.AutoSize = true;
-            this.actionControl.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.actionControl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.actionControl.IsTopLevel = false;
-            this.actionControl.Location = new System.Drawing.Point(7, 21);
-            this.actionControl.Margin = new System.Windows.Forms.Padding(5);
-            this.actionControl.MinimumSize = new System.Drawing.Size(400, 39);
-            this.actionControl.Name = "actionControl";
-            this.actionControl.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
-            this.actionControl.Size = new System.Drawing.Size(486, 133);
-            this.actionControl.TabIndex = 0;
+            this.tlpContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlpContainer.ColumnCount = 2;
+            this.tlpContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpContainer.Controls.Add(this.grpAction, 1, 0);
+            this.tlpContainer.Controls.Add(this.grpTrigger, 0, 0);
+            this.tlpContainer.Location = new System.Drawing.Point(14, 47);
+            this.tlpContainer.Name = "tlpContainer";
+            this.tlpContainer.RowCount = 1;
+            this.tlpContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpContainer.Size = new System.Drawing.Size(1024, 154);
+            this.tlpContainer.TabIndex = 93;
             // 
             // MappingControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.lblHeader);
-            this.Controls.Add(this.grpTrigger);
-            this.Controls.Add(this.grpAction);
+            this.Controls.Add(this.tlpContainer);
             this.Controls.Add(this.chkCreateReverseMapping);
             this.Controls.Add(this.btnSaveMapping);
             this.Name = "MappingControl";
@@ -147,6 +162,7 @@ partial class MappingControl
             this.grpAction.PerformLayout();
             this.grpTrigger.ResumeLayout(false);
             this.grpTrigger.PerformLayout();
+            this.tlpContainer.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,4 +176,5 @@ partial class MappingControl
     private Mapping.TriggerControl triggerControl;
     private System.Windows.Forms.CheckBox chkCreateReverseMapping;
     private System.Windows.Forms.Label lblHeader;
+    private System.Windows.Forms.TableLayoutPanel tlpContainer;
 }
