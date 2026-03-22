671f8c108f6e818500cb300da6be1cafa22cb2df
luttje <2738114+luttje@users.noreply.github.com>
2026-03-22 14:32:30 +0100

    Remove unncessary usings

--- Core/Key2Joy.Contracts/Mapping/CallbackAction.cs
+++ Core/Key2Joy.Contracts/Mapping/CallbackAction.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Key2Joy.Contracts.Mapping;
 
 public delegate void CallbackAction(params object[] arguments);
