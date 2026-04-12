d86dc4a7bf41f75343cbaba70102934d9e51f301
luttje <2738114+luttje@users.noreply.github.com>
2026-04-12 15:51:27 +0200

    Refactor mapping list to UserControl

--- Key2Joy.App/Pages/MainMappingPage.xaml.cs
+++ Key2Joy.App/Pages/MainMappingPage.xaml.cs
@@ -4,8 +4,6 @@ using Key2Joy.Mapping.Actions.Logic;
 using Key2Joy.Mapping.Triggers.Mouse;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Media;
 
 namespace Key2Joy.App.Pages;
 
@@ -45,74 +43,10 @@ public sealed partial class MainMappingPage : Page, IAcceptAppCommands
         this.MappingControl.SelectMapping(null);
     }
 
-    private void MappingItem_Tapped(object sender, Microsoft.UI.Xaml.Input.TappedRoutedEventArgs e)
+    private void MappingGroupsList_MappingSelected(object sender, MappedOption option)
     {
-        var element = sender as FrameworkElement;
-        var vm = element?.Tag as MappedOptionViewModel;
-        this.ViewModel.SelectMapping(vm?.Option);
-        this.MappingControl.SelectMapping(vm?.Option);
-    }
-
-    private void MappingItem_DoubleTapped(object sender, Microsoft.UI.Xaml.Input.DoubleTappedRoutedEventArgs e)
-    {
-        var element = sender as FrameworkElement;
-        var stackPanel = element?.Parent as StackPanel;
-
-        if (stackPanel == null || stackPanel.Children.Count < 2)
-        {
-            return;
-        }
-
-        var childrenPanel = stackPanel.Children[1] as ItemsControl;
-
-        if (childrenPanel == null)
-        {
-            return;
-        }
-
-        ToggleButton? toggleButton = null;
-
-        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++)
-        {
-            if (VisualTreeHelper.GetChild(element, i) is ToggleButton tb)
-            {
-                toggleButton = tb;
-                break;
-            }
-        }
-
-        if (toggleButton == null || toggleButton.Visibility == Visibility.Collapsed)
-        {
-            return;
-        }
-
-        SetExpanded(toggleButton, childrenPanel, !toggleButton.IsChecked.Value);
-    }
-
-    private void ExpandToggle_Click(object sender, RoutedEventArgs e)
-    {
-        if (sender is not ToggleButton toggleButton)
-        {
-            return;
-        }
-
-        // Find the children ItemsControl, it's the second child of the parent StackPanel
-        var parentRow = toggleButton.Parent as FrameworkElement;
-        var stackPanel = parentRow?.Parent as StackPanel;
-
-        if (stackPanel == null)
-        {
-            return;
-        }
-
-        var childrenPanel = stackPanel.Children[1] as ItemsControl;
-
-        if (childrenPanel == null)
-        {
-            return;
-        }
-
-        SetExpanded(toggleButton, childrenPanel, toggleButton.IsChecked == true);
+        this.ViewModel.SelectMapping(option);
+        this.MappingControl.SelectMapping(option);
     }
 
     public bool RunAppCommand(AppCommand command)
@@ -164,109 +98,11 @@ public sealed partial class MainMappingPage : Page, IAcceptAppCommands
         this.ViewModel.HandleMappingDeleted(option);
     }
 
-    public void ExpandAllMappings() => this.SetAllMappingsExpanded(true);
+    public void ExpandAllMappings() => this.MappingGroupsList.ExpandAll();
 
-    public void CollapseAllMappings() => this.SetAllMappingsExpanded(false);
+    public void CollapseAllMappings() => this.MappingGroupsList.CollapseAll();
 
-    private void SetAllMappingsExpanded(bool expanded)
-    {
-        var groupPanel = this.MappingGroupsControl.ItemsPanelRoot;
-
-        if (groupPanel == null)
-        {
-            return;
-        }
-
-        foreach (var groupChild in groupPanel.Children)
-        {
-            var groupStackPanel = FindFirstChild<StackPanel>(groupChild);
-
-            if (groupStackPanel == null || groupStackPanel.Children.Count < 2)
-            {
-                continue;
-            }
-
-            var itemsControl = groupStackPanel.Children[1] as ItemsControl;
-            var itemsPanel = itemsControl?.ItemsPanelRoot;
-
-            if (itemsPanel == null)
-            {
-                continue;
-            }
-
-            foreach (var itemChild in itemsPanel.Children)
-            {
-                var itemStackPanel = FindFirstChild<StackPanel>(itemChild);
-
-                if (itemStackPanel == null || itemStackPanel.Children.Count < 2)
-                {
-                    continue;
-                }
-
-                var parentRow = itemStackPanel.Children[0] as Grid;
-                var childrenPanel = itemStackPanel.Children[1] as ItemsControl;
-
-                if (parentRow == null || childrenPanel == null)
-                {
-                    continue;
-                }
-
-                ToggleButton? toggleButton = null;
-
-                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parentRow); i++)
-                {
-                    if (VisualTreeHelper.GetChild(parentRow, i) is ToggleButton tb)
-                    {
-                        toggleButton = tb;
-                        break;
-                    }
-                }
-
-                if (toggleButton == null || toggleButton.Visibility == Visibility.Collapsed)
-                {
-                    continue;
-                }
-
-                SetExpanded(toggleButton, childrenPanel, expanded);
-            }
-        }
-    }
-
-    private static T? FindFirstChild<T>(DependencyObject parent) where T : DependencyObject
-    {
-        var count = VisualTreeHelper.GetChildrenCount(parent);
-
-        for (int i = 0; i < count; i++)
-        {
-            var child = VisualTreeHelper.GetChild(parent, i);
-
-            if (child is T result)
-            {
-                return result;
-            }
-
-            var found = FindFirstChild<T>(child);
-
-            if (found != null)
-            {
-                return found;
-            }
-        }
-
-        return null;
-    }
 
     public void SetSelectedProfile(MappingProfile profile)
         => this.ViewModel.SetSelectedProfile(profile);
-
-    private static void SetExpanded(ToggleButton toggleButton, ItemsControl childrenPanel, bool expanded)
-    {
-        toggleButton.IsChecked = expanded;
-        childrenPanel.Visibility = expanded ? Visibility.Visible : Visibility.Collapsed;
-
-        if (toggleButton.Content is FontIcon icon)
-        {
-            icon.Glyph = expanded ? "\xE96D" : "\xE96E";
-        }
-    }
 }
---
+++ Key2Joy.App/UserControls/MappingGroupsListControl.xaml.cs
@@ -0,0 +1,195 @@
+using System;
+using System.Collections.ObjectModel;
+using Key2Joy.App.Pages;
+using Key2Joy.Mapping;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Media;
+using DependencyPropertyGenerator;
+
+namespace Key2Joy.App.UserControls;
+
+[DependencyProperty<ObservableCollection<MappingGroupViewModel>>("FilteredMappingGroups")]
+[DependencyProperty<int>("FilteredMappedOptionsCount")]
+public sealed partial class MappingGroupsListControl : UserControl
+{
+    public event EventHandler<MappedOption> MappingSelected;
+
+    public MappingGroupsListControl()
+    {
+        this.InitializeComponent();
+    }
+
+    public void ExpandAll() => this.SetAllMappingsExpanded(true);
+
+    public void CollapseAll() => this.SetAllMappingsExpanded(false);
+
+    private void MappingItem_Tapped(object sender, Microsoft.UI.Xaml.Input.TappedRoutedEventArgs e)
+    {
+        var element = sender as FrameworkElement;
+        var vm = element?.Tag as MappedOptionViewModel;
+        this.MappingSelected?.Invoke(this, vm?.Option);
+    }
+
+    private void MappingItem_DoubleTapped(object sender, Microsoft.UI.Xaml.Input.DoubleTappedRoutedEventArgs e)
+    {
+        var element = sender as FrameworkElement;
+        var stackPanel = element?.Parent as StackPanel;
+
+        if (stackPanel == null || stackPanel.Children.Count < 2)
+        {
+            return;
+        }
+
+        var childrenPanel = stackPanel.Children[1] as ItemsControl;
+
+        if (childrenPanel == null)
+        {
+            return;
+        }
+
+        ToggleButton? toggleButton = null;
+
+        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++)
+        {
+            if (VisualTreeHelper.GetChild(element, i) is ToggleButton tb)
+            {
+                toggleButton = tb;
+                break;
+            }
+        }
+
+        if (toggleButton == null || toggleButton.Visibility == Visibility.Collapsed)
+        {
+            return;
+        }
+
+        SetExpanded(toggleButton, childrenPanel, !toggleButton.IsChecked.Value);
+    }
+
+    private void ExpandToggle_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is not ToggleButton toggleButton)
+        {
+            return;
+        }
+
+        // Find the children ItemsControl, it's the second child of the parent StackPanel
+        var parentRow = toggleButton.Parent as FrameworkElement;
+        var stackPanel = parentRow?.Parent as StackPanel;
+
+        if (stackPanel == null)
+        {
+            return;
+        }
+
+        var childrenPanel = stackPanel.Children[1] as ItemsControl;
+
+        if (childrenPanel == null)
+        {
+            return;
+        }
+
+        SetExpanded(toggleButton, childrenPanel, toggleButton.IsChecked == true);
+    }
+
+    private void SetAllMappingsExpanded(bool expanded)
+    {
+        var groupPanel = this.MappingGroupsControl.ItemsPanelRoot;
+
+        if (groupPanel == null)
+        {
+            return;
+        }
+
+        foreach (var groupChild in groupPanel.Children)
+        {
+            var groupStackPanel = FindFirstChild<StackPanel>(groupChild);
+
+            if (groupStackPanel == null || groupStackPanel.Children.Count < 2)
+            {
+                continue;
+            }
+
+            var itemsControl = groupStackPanel.Children[1] as ItemsControl;
+            var itemsPanel = itemsControl?.ItemsPanelRoot;
+
+            if (itemsPanel == null)
+            {
+                continue;
+            }
+
+            foreach (var itemChild in itemsPanel.Children)
+            {
+                var itemStackPanel = FindFirstChild<StackPanel>(itemChild);
+
+                if (itemStackPanel == null || itemStackPanel.Children.Count < 2)
+                {
+                    continue;
+                }
+
+                var parentRow = itemStackPanel.Children[0] as Grid;
+                var childrenPanel = itemStackPanel.Children[1] as ItemsControl;
+
+                if (parentRow == null || childrenPanel == null)
+                {
+                    continue;
+                }
+
+                ToggleButton? toggleButton = null;
+
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parentRow); i++)
+                {
+                    if (VisualTreeHelper.GetChild(parentRow, i) is ToggleButton tb)
+                    {
+                        toggleButton = tb;
+                        break;
+                    }
+                }
+
+                if (toggleButton == null || toggleButton.Visibility == Visibility.Collapsed)
+                {
+                    continue;
+                }
+
+                SetExpanded(toggleButton, childrenPanel, expanded);
+            }
+        }
+    }
+
+    private static T? FindFirstChild<T>(DependencyObject parent) where T : DependencyObject
+    {
+        var count = VisualTreeHelper.GetChildrenCount(parent);
+
+        for (int i = 0; i < count; i++)
+        {
+            var child = VisualTreeHelper.GetChild(parent, i);
+
+            if (child is T result)
+            {
+                return result;
+            }
+
+            var found = FindFirstChild<T>(child);
+
+            if (found != null)
+            {
+                return found;
+            }
+        }
+
+        return null;
+    }
+
+    private static void SetExpanded(ToggleButton toggleButton, ItemsControl childrenPanel, bool expanded)
+    {
+        toggleButton.IsChecked = expanded;
+        childrenPanel.Visibility = expanded ? Visibility.Visible : Visibility.Collapsed;
+
+        if (toggleButton.Content is FontIcon icon)
+        {
+            icon.Glyph = expanded ? "\xE96D" : "\xE96E";
+        }
+    }
+}
